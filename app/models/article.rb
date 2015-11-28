18e60c07e51cbf6f4aaa2dc739581ee6ca719604
John Kim <johntkim92@gmail.com>
2015-11-28 17:26:00 -0500

    comment model up with proper relationship to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :cash, presence: true
+  validates :start_date, presence: true
+
+  has_many :comments, dependent: :destroy
+
 end
